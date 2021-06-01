6c94c032b77527541390d881cf250a36e0f73095
evgeniic <>
2021-06-01 15:01:46 +0300

    database pre ping

--- chatbot_ru/talker/db/database.py
+++ chatbot_ru/talker/db/database.py
@@ -9,7 +9,7 @@ db_name = getenv('DB_NAME')
 db_port = getenv('DB_PORT')
 db_host = getenv('DB_HOST')
 SQLALCHEMY_DATABASE_URL = f'postgresql://{db_user}:{db_pass}@{db_host}:{db_port}/{db_name}'
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
+engine = create_engine(SQLALCHEMY_DATABASE_URL, pool_pre_ping=True)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 
