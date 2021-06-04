09fdaa9f533193680d8d18c2bc93acdcb3a47a2e
evgeniic <>
2021-06-04 16:27:50 +0300

    single chat inference

--- chatbot_ru/talker/main.py
+++ chatbot_ru/talker/main.py
@@ -1,9 +1,5 @@
 
 import logging
-import time
-import numpy as np
-import threading
-from queue import Empty, Queue
 from inference_models.nlp import NLP, ModelLoader
 import Log
 from fastapi import FastAPI, Depends
@@ -20,13 +16,8 @@ logger = Log.get_logger()
 
 model = ModelLoader().load_model(NLP(), inf_config['inference_model'] )
 app = FastAPI()
-requests_queue = Queue()
 logger.info(f"Application started")
 
-
-BATCH_SIZE = 3
-BATCH_TIMEOUT = 0.5
-CHECK_INTERVAL = 0.01
 
 def get_db():
     db_ = database.SessionLocal()
@@ -45,38 +36,13 @@ def answer(conv: schemas.Conversation, db_: Session = Depends(get_db)):
         history = crud.get_chat(db=db_, chat_id=conv.chat_id).conv_text
         if history and len(history) > 500:
             crud.update_conversation(db=db_, chat_id=conv.chat_id, conv_text=None) 
-            return {"text": "Так о чем это мы? Давай начнем сначала. Напиши что-нибудь."}  
-        request = {'input': conv.conv_text, 'history': history, 'time': time.time()}
-        requests_queue.put(request)
-
-        while 'output' not in request:
-            time.sleep(CHECK_INTERVAL)
-        crud.update_conversation(db=db_, chat_id=conv.chat_id, conv_text=request['new_history'])  
-        return {'text': request['output']} 
-    except Exception as e:
-        logger.error(f"Error while processing message. Error msg: {e}")
-        logger.exception(e) 
-
-def handle_requests_by_batch():
-    while True:
-        requests_batch = []
-        while not (
-            len(requests_batch) > BATCH_SIZE or
-            (len(requests_batch) > 0 and time.time() - requests_batch[0]['time'] > BATCH_TIMEOUT)
-        ):
-            try:
-                requests_batch.append(requests_queue.get(timeout=CHECK_INTERVAL))
-            except Empty:
-                continue
-        logger.info(f'Batch size: {len(requests_batch)}')
-        batch_inputs = [request['input'] for request in requests_batch]
-        batch_history = [request['history'] for request in requests_batch]
+            return {"text": "Так о чем это мы? Давай начнем сначала. Напиши что-нибудь."}
         start = timer()
-        batch_outputs, batch_new_history = model.get_answer_ru(batch_inputs, batch_history)
+        answer, new_history = model.get_answer_ru([conv.conv_text], [history])
         end = timer()
         logger.info(f'Get answer from model: {end - start}')
-        for request, output, new_history in zip(requests_batch, batch_outputs, batch_new_history):
-            request['output'] = output
-            request['new_history'] = new_history
-
-threading.Thread(target=handle_requests_by_batch).start()
+        crud.update_conversation(db=db_, chat_id=conv.chat_id, conv_text=new_history[0])  
+        return {"text": answer[0] if answer[0] else "мне нечем ответить"}
+    except Exception as e:
+        logger.error(f"Error while processing message. Error msg: {e}")
+        logger.exception(e)  
