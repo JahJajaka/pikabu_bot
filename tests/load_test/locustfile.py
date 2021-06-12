fd5c377059eebc8ba3165f79db6986c0de55359c
iney2 <panov20@gmail.com>
2021-06-12 19:47:32 +1000

    lower timeout added to tests

--- tests/load_test/locustfile.py
+++ tests/load_test/locustfile.py
@@ -6,13 +6,13 @@ import random
 
 class QuickstartUser(HttpUser):
     wait_time = between(1, 5)
-    test_df = load_test.build_dataframe(10)
+    test_df = load_test.build_dataframe(20)
 
     @task
     def hello_world(self):      
         for col in self.test_df.columns:
             for message in self.test_df[col]:               
                 self.client.post("/message", json={"conv_text":message, "chat_id": int(random.choice(self.test_df.columns))}, headers={'Connection':'close'})
-                time.sleep(1)
+                time.sleep(0.1)
 
 
